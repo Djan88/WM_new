e587772f160c871d010c2826504f98bf10fac4a1
Ivan Avraamov <ivan.avraamov@gmail.com>
2021-05-21 14:30:14 +0300

    d-7

--- wp-content/themes/bcwish/js/protocols_test.js
+++ wp-content/themes/bcwish/js/protocols_test.js
@@ -16329,9 +16329,9 @@ mmt_2 = function(){
         zIndex: '1000'
     });
     jQuery('.zone_v2').css({background: '#fff url(/wp-content/themes/bcwish/img/super_plod.png) center center/100% no-repeat'});
-    jQuery('.zone_d2_').addClass('rot_90_one').css({background: '#fff url(/wp-content/themes/bcwish/img/d_.png) center center/100% no-repeat'});
+    jQuery('.zone_d2_').addClass('rot_d_one').css({background: '#fff url(/wp-content/themes/bcwish/img/d_.png) center center/100% no-repeat'});
     phaseOne = setInterval(function(){
-      if (count_animation <= 32){
+      if (count_animation <= 40){
         if (reloadTime == 0){                                                                       //1
             sound.stop();
             reloadSound.play();
