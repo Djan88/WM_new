30a514f5798f896d72cb6a3afc14c602fd4bda55
Ivan Avraamov <ivan.avraamov@gmail.com>
2022-04-08 17:40:39 +0300

    mobile_karma-2

--- wp-content/themes/mobile/js/protocols.js
+++ wp-content/themes/mobile/js/protocols.js
@@ -16034,10 +16034,10 @@ checkPoints = function(){
 // START
 jQuery('.btn_start').on('click', function(event) {
   checkPoints();
-  if(pointsStatus == false){
-    swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести на фото все зоны", "info");
-    pointsStatus = true;
-  } else {
+  // if(pointsStatus == false){
+  //   swal("Не все зоны перенесены", "Перед началом процедуры необходимо перенести на фото все зоны", "info");
+  //   pointsStatus = true;
+  // } else {
     jQuery('.header-title').text('Программа выполняется');
     jQuery('.zone').css('background', 'rgba(83,35,69, 0.4)');
     jQuery('.btn-to_protocols').addClass('hidden');
@@ -16094,7 +16094,7 @@ jQuery('.btn_start').on('click', function(event) {
       jQuery('.header-title').text('Программа выполняется');
       jQuery('.btn_start').attr('disabled', 'disabled');
     }
-  }
+  // }
 });
 // STOP
 function hideNote() {
