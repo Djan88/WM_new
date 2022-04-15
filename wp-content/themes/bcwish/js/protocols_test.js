81bdff016f79048c6d18ad7f8a4678ee106c6578
Ivan Avraamov <ivan.avraamov@gmail.com>
2022-04-15 15:14:03 +0300

    new-design-75

--- wp-content/themes/bcwish/js/protocols_test.js
+++ wp-content/themes/bcwish/js/protocols_test.js
@@ -24827,7 +24827,7 @@ mmt_2 = function(){
   }
 
   jQuery('.wizard_stop') .on('click', function(event) {
-    console.log('DIN');
+    console.log(protocol);
     if (!protocol == 'moon') {
       jQuery('.wizard_stop').addClass('wizard_stop_inProgress');
       jQuery('.header-title').text('Программа останавливается');
